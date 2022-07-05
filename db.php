caa0532c076e3f53610b3c5725e67896fb799b09
TeeZeeSak <enzo80012@gmail.com>
2022-07-05 23:38:11 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "localhost";
-$username = "teezxyz_website";
-$password = "721011715bS";
-$database = "teezxyz_steamwatch";
+$username = "user";
+$password = "pass";
+$database = "db";
 ?>
